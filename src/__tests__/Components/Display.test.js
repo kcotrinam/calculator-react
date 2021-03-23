ef68f52d393faaf6fb8d84d0586929e249988e40
Kevin Cotrina <kcotrinma92@gmail.com>
2021-03-23 00:46:48 -0500

    Add tests for components

---
+++ src/__tests__/Components/Display.test.js
@@ -0,0 +1,15 @@
+import { create } from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Display from '../../components/Display';
+
+test('renders correctly', () => {
+  const tree = create(<Display result="test" />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('Displays 6', () => {
+  render(<Display result="6" />);
+  const number = screen.getByText('6');
+  expect(number).toBeInTheDocument();
+});
